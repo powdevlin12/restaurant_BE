b4c5b25a8e2eb256efd4a8725cac4a29e8b78998
DyNamite-TNT-1 <tdhuaco372001@gmail.com>
2023-08-17 15:07:26 +0700

    add get tabletype api

--- src/routers/table.routers.js
+++ src/routers/table.routers.js
@@ -6,12 +6,15 @@ const { authorize } = require("../middlewares/auth/authorize.js");
 const {
   getAllTableByDate,
   checkAvailableTable,
+  getAllTableType
 } = require("../controllers/table.controllers");
 const tableRouter = express.Router();
 
 tableRouter.get("/get", getAllTableByDate);
 
 tableRouter.get("/check/available", checkAvailableTable);
+
+tableRouter.get("/get/type/all", getAllTableType);
 
 module.exports = {
   tableRouter,
